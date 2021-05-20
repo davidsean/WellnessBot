e334ed9883dffafa7d6c4d29088ce98f669b5e3e
cSDes1gn <christian911@sympatico.ca>
2021-05-20 12:23:41 -0400

    build and exec wellness bot image

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 source .env
+
+printf "%b" "${OKB}Building wellness-bot image${NC}\n"
 docker build -t wellness-bot:latest \
     -f docker/arm/Dockerfile \
     --no-cache .
+printf "%b" "${OKG} ✓ ${NC}image built\n"
+
+printf "%b" "${OKB}Starting wellness-bot server${NC}\n"
+docker run --env-file .env -d wellness-bot
+printf "%b" "${OKG} ✓ ${NC}server container started\n"
