71b102cfc91d878ade6d3ffe5a274f801ab2dece
cSDes1gn <christian911@sympatico.ca>
2021-05-23 15:25:06 -0400

    for testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,5 +7,9 @@ source .env
 export DISCORD_TOKEN="$DISCORD_TOKEN"
 export DISCORD_GUILD_ID="$DISCORD_GUILD_ID"
 export CHANNEL_ID="$CHANNEL_ID"
+export MONGO_PASS="$MONGO_PASS"
+export MONGO_USER="$MONGO_USER"
+export MONGO_PORT="$MONGO_PORT"
+export MONGO_HOST=localhost
 
 python3 -m app
