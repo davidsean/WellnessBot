33a90152416f12f225cffea7cf229ad2f0d7995c
cSDes1gn <christian911@sympatico.ca>
2021-06-25 01:01:08 -0400

    do not run as daemon in dev env

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -8,8 +8,8 @@ trap 'handler $? $LINENO' ERR
 handler () {
     printf "%b" "${FAIL} ✗ ${NC} dist build failed on line $2 with exit status $1\n"
 }
-# TODO: add context for arm64 vs x86 hosts (arm64 for deployment and x86 for dev)
-printf "%b" "${OKB}Building wellness-bot for x86 host${NC}\n"
-docker compose -f docker/docker-compose.yaml up -d
+
+printf "%b" "${OKB}Building wellness-bot for dev${NC}\n"
+docker compose -f docker/docker-compose.yaml up
 printf "%b" "${OKG} ✓ ${NC}containers active\n"
 printf "%b" "${OKB}To teardown the containers run: docker compose -f docker/docker-compose.yaml down${NC}\n"
